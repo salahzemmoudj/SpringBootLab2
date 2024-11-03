ca063134c331d1d280b7b96c0e0e7ed18e576820
salah <salah.zemmoudj@univ-bejaia.dz>
2024-11-03 15:32:13 +0100

    Construction de projet

---
+++ src/main/java/com/autourducode/SpringBootLab2/SpringBootLab2Application.java
@@ -0,0 +1,13 @@
+package com.autourducode.SpringBootLab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLab2Application.class, args);
+	}
+
+}
